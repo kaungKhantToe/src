a1de9e165084732c760921901865bcf9e48a7291
Kaung Khant Toe <toekaungkhant753@gmail.com>
2023-10-14 04:31:17 -0400

    modified:   projects/CodingChallenge_01.java

--- projects/CodingChallenge_01.java
+++ projects/CodingChallenge_01.java
@@ -13,17 +13,17 @@ public class CodingChallenge_01 {
         System.out.println("Please enter the desired text to determine grade level :");
         String userInput = input.nextLine();
         for( int i = 0; i < userInput.length(); i++){
-            letterCount++;
+            letterCount++; // Increment the value of the variable with each letter in the input of the user
             if(userInput.charAt(i) == ' '){
-                wordCount++;
-                letterCount--;
+                wordCount++; // Each space will be considered as one word 
+                letterCount--; // Because we don't want to count null characters as actual letters
             }
             if(userInput.charAt(i) == '.' || userInput.charAt(i) == '!' || userInput.charAt(i) == '?' ){
-                sentenceCount++;
-                letterCount--;
+                sentenceCount++;// most sentences end with these characters
+                letterCount--; 
             }
         }
-        wordCount ++;
+        wordCount ++; // Since the last word cannot be counted by looking for null characters (like we did above) , we will have to add one ourselves 
         Double S = (double)(sentenceCount)/wordCount*100;
         Double L = (double)(letterCount)/wordCount*100;
         int gradeLevel = (int)((0.0588 * L) - (0.296 * S) - 15.8);
