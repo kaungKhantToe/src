483750f8674e0ec25768138b11bc69d473fc7246
Kaung Khant Toe <toekaungkhant753@gmail.com>
2023-11-19 16:25:59 -0500

    modified:   projects/CodingChallenge_03.java

--- projects/CodingChallenge_03.java
+++ projects/CodingChallenge_03.java
@@ -27,30 +27,67 @@ public class CodingChallenge_03 {
         String[] rankTwoCandidates = new String[numberOfVoters];
         String[] rankThreeCandidates = new String[numberOfVoters];
 
+        double favorForAlice = 0;
+        double favorForBob = 0;
+        double favorForCharlie = 0;
+
+        Scanner input = new Scanner(System.in);
+
         try {
-            Scanner input = new Scanner(System.in);
             /*
              * a for loop that accepts the ranks of each candidate and stores
              * them in their respective arrays based on the voter's preference
              */
             for (int i = 0; i < numberOfVoters; i++) {
                 System.out.print("Rank 1: ");
-                rankOneCandidates[i] = input.next().strip();
+                rankOneCandidates[i] = input.next().strip().toLowerCase();
 
                 System.out.print("Rank 2: ");
-                rankTwoCandidates[i] = input.next().strip();
+                rankTwoCandidates[i] = input.next().strip().toLowerCase();
 
                 System.out.print("Rank 3: ");
-                rankThreeCandidates[i] = input.next().strip();
+                rankThreeCandidates[i] = input.next().strip().toLowerCase();
 
                 System.out.println(); // *to format the output as depicted in the example
+
             }
-            
+
+            favorForAlice = ranking(rankOneCandidates, "alice") + 0.5 * ranking(rankTwoCandidates, "alice")
+                    + 0.5 * 0.5 * ranking(rankThreeCandidates, "Alice");
+
+            favorForBob = ranking(rankOneCandidates, "bob") + 0.5 * ranking(rankTwoCandidates, "bob")
+                    + 0.5 * 0.5 * ranking(rankThreeCandidates, "Bob");
+
+            favorForCharlie = ranking(rankOneCandidates, "charlie") + 0.5 * ranking(rankTwoCandidates, "charlie")
+                    + 0.5 * 0.5 * ranking(rankThreeCandidates, "charlie");
+
+            // System.out.println(favorForAlice);
+            // System.out.println(favorForBob);
+            // System.out.println(favorForCharlie);
+            if (Math.max(favorForAlice, Math.max(favorForBob, favorForCharlie)) == favorForAlice) {
+                System.out.println("Alice");
+            } else if (Math.max(favorForBob, Math.max(favorForAlice, favorForCharlie)) == favorForBob) {
+                System.out.println("Bob");
+            } else if (Math.max(favorForCharlie, Math.max(favorForAlice, favorForBob)) == favorForCharlie) {
+                System.out.println("Charlie");
+            }
+
         } catch (Exception e) {
 
+        } finally {
+            input.close();
         }
 
     }
 
-    
+    private static int ranking(String[] candidate, String name) {
+        int ranks = 0;
+        for (int i = 0; i < candidate.length; i++) {
+            if (candidate[i].equalsIgnoreCase(name)) {
+                ranks++;
+            }
+        }
+        return ranks;
+    }
+
 }
