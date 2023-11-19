3e513ad398296aba6606d02f1274cfb098a466af
Kaung Khant Toe <toekaungkhant753@gmail.com>
2023-11-19 15:47:04 -0500

    modified:   projects/CodingChallenge_03.java

--- projects/CodingChallenge_03.java
+++ projects/CodingChallenge_03.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class CodingChallenge_03 {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Number of voters: ");
+
         // * an array that holds the names of candidates
         String[] listOfCandidates = { "Alice", "Bob", "Charlie" };
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Number of voters: ");
+
         try {
             int numberOfVoters = input.nextInt();
             // *
@@ -32,66 +35,22 @@ public class CodingChallenge_03 {
              */
             for (int i = 0; i < numberOfVoters; i++) {
                 System.out.print("Rank 1: ");
-                rankOneCandidates[i] = input.next();
+                rankOneCandidates[i] = input.next().strip();
 
                 System.out.print("Rank 2: ");
-                rankTwoCandidates[i] = input.next();
+                rankTwoCandidates[i] = input.next().strip();
 
                 System.out.print("Rank 3: ");
-                rankThreeCandidates[i] = input.next();
+                rankThreeCandidates[i] = input.next().strip();
 
                 System.out.println(); // *to format the output as depicted in the example
             }
-            /*
-             * 
-             */
-            rankings(rankOneCandidates, listOfCandidates, 1);
-            rankings(rankTwoCandidates, listOfCandidates, 2);
-            rankings(rankThreeCandidates, listOfCandidates, 3);
-
+            
         } catch (Exception e) {
 
         }
 
     }
 
-    private static double rankings(String[] candidates, String[] listOfCandidates, int rank) {
-        double favorForAlice = 0;
-        double favorForBob = 0;
-        double favorForCharlie = 0;
-        for (int i = 0; i < 3; i++) {
-            if (rank == 1) {
-                if (candidates[i].equalsIgnoreCase(listOfCandidates[0])) {
-                    favorForAlice += 1;
-                } else if (candidates[i].equalsIgnoreCase(listOfCandidates[1])) {
-                    favorForBob += 1;
-                } else if (candidates[i].equalsIgnoreCase(listOfCandidates[2])) {
-                    favorForCharlie += 1;
-                }
-            } else if (rank == 2) {
-                if (candidates[i].equalsIgnoreCase(listOfCandidates[0])) {
-                    favorForAlice += 0.5;
-                } else if (candidates[i].equalsIgnoreCase(listOfCandidates[1])) {
-                    favorForBob += 0.5;
-                } else if (candidates[i].equalsIgnoreCase(listOfCandidates[2])) {
-                    favorForCharlie += 0.5;
-                }
-
-            }
-
-        }
-        
-        return favorForAlice;
-    }
-    // System.out.println(favorForBob);
-    // System.err.println(favorForCharlie);
-
-    /* 
-        if (favorForAlice > favorForBob && favorForAlice > favorForCharlie) {
-            System.out.print("Alice");
-        } else if (favorForBob > favorForAlice && favorForBob > favorForCharlie) {
-            System.out.print("Bob");
-        }
-        */
-
+    
 }
