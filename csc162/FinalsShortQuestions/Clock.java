bcc973e59385e9cd48e7b7659572ced92f561532
Kaung Khant Toe <toekaungkhant753@gmail.com>
2023-12-16 11:06:39 -0500

    modified:   csc162/FinalsShortQuestions/Clock.java

--- csc162/FinalsShortQuestions/Clock.java
+++ csc162/FinalsShortQuestions/Clock.java
@@ -1,4 +1,5 @@
 package FinalsShortQuestions;
+
 public class Clock {
     private int hr;
     private int min;
@@ -14,9 +15,9 @@ public class Clock {
         this.sec = sec;
     }
 
-    public void setTime(int hr,int min, int sec) {
+    public void setTime(int hr, int min, int sec) {
         this.hr = hr;
-        this.min =min;
+        this.min = min;
         this.sec = sec;
     }
 
@@ -30,6 +31,44 @@ public class Clock {
 
     public int getSeconds() {
         return sec;
+    }
+
+    public void printTime() {
+        System.out
+                .print("The time as of now in the form of Hour, Minutes and Seconds is " + hr + " " + min + " " + sec);
+    }
+
+    public int incrementHours(int hr) {
+        return hr++;
+    }
+
+    public int incrementMinutes(int min) {
+        return min++;
+    }
+
+    public int incrementSeconds(int sec) {
+        return sec++;
+    }
+
+    public boolean equals(Clock clock) {
+        return (sec == clock.hr
+                && sec == clock.min
+                && sec == clock.sec);
+    }
+
+    public void makeCopy(Clock clock) {
+        hr = clock.hr;
+        min = clock.min;
+        sec = clock.sec;
+    }
+
+    public Clock getCopy() {
+        Clock temp = new Clock();
+        temp.hr = hr;
+        temp.min = min;
+        temp.sec = sec;
+
+        return temp;
     }
 
 }
